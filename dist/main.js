0be95e0928da88cc317e42249f207dcf19e68588
Tyler Gardiner <78093138+tyler-g-AVGroup@users.noreply.github.com>
2022-05-31 08:40:35 -0400

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -26,6 +26,7 @@ function uploadMuhFile(filePath) {
         .catch(function (error) {
         if (isDebug)
             console.error(error);
+        core.setOutput('Error', error);
         return error;
     });
 }
@@ -38,5 +39,6 @@ glob(globSource, {}, function (err, files) {
     })
         .catch(function (err) {
         console.error('error', err);
+        core.setOutput('Error', err);
     });
 });
